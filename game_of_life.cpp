4148917228f32b18a81c673c8f7aa06de42ff409
Anwesha Sen <anwesha.sen321@gmail.com>
2020-10-20 08:35:09 +0530

    Added option to enter input from text file

--- game_of_life.cpp
+++ game_of_life.cpp
@@ -2,9 +2,10 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
 
-#define row 40
-#define column 100
+int row = 40;
+int column = 100;
 #define refresh_rate 100 //time in milliseconds after which next generation is produced
 
 std::vector<std::vector<int>> slate(row, std::vector<int>(column, 0));
@@ -18,12 +19,21 @@ std::vector<std::vector<int>> slate(row, std::vector<int>(column, 0));
 void init_slate();
 void display_slate();
 void next_gen();
+bool validateArgs(int argc, char **argv);
+void init_slate_from_file(char *path);
 
-int main()
+int main(int argc, char **argv)
 {
-    int generation = 1;
-    init_slate();
+    if (argc > 1 && validateArgs(argc, argv))
+    {
+        init_slate_from_file(argv[1]);
+    }
+    else
+    {
+        init_slate();
+    }
     display_slate();
+    int generation = 1;
     std::cout << "Generation: " << generation << std::endl;
     clock_t start = clock();
     while (true)
@@ -44,13 +54,15 @@ int main()
 void init_slate()
 {
     srand(time(NULL));
+    slate.resize(row);
     for (int i = 0; i < row; i++)
     {
+        slate[i].resize(column);
         for (int j = 0; j < column; j++)
             if (rand() % 2)
-                slate[i][j] = 1;        // Live state
+                slate[i][j] = 1; // Live state
             else
-                slate[i][j] = 3;        // Inhabitated state
+                slate[i][j] = 3; // Inhabitated state
     }
 }
 
@@ -80,48 +92,109 @@ void display_slate()
 
 void next_gen()
 {
-    std::vector<std::vector<int>> val_neighbour(row, std::vector<int>(column,0));
-    for(int i=0; i<row; i++)
+    std::vector<std::vector<int>> val_neighbour(row, std::vector<int>(column, 0));
+    for (int i = 0; i < row; i++)
     {
-        for(int j=0; j<column; j++)
+        for (int j = 0; j < column; j++)
         {
             int neighbours = 0,
                 left = (j - 1 + column) % column,
                 right = (j + 1 + column) % column,
                 top = (i - 1 + row) % row,
                 bottom = (i + 1 + row) % row;
-            if(slate[top][left] == 1)
+            if (slate[top][left] == 1)
                 ++neighbours;
-            if(slate[top][j] == 1)
+            if (slate[top][j] == 1)
                 ++neighbours;
-            if(slate[top][right] == 1)
+            if (slate[top][right] == 1)
                 ++neighbours;
-            if(slate[i][left] == 1)
+            if (slate[i][left] == 1)
                 ++neighbours;
-            if(slate[i][right] == 1)
+            if (slate[i][right] == 1)
                 ++neighbours;
-            if(slate[bottom][left] == 1)
+            if (slate[bottom][left] == 1)
                 ++neighbours;
-            if(slate[bottom][j] == 1)
+            if (slate[bottom][j] == 1)
                 ++neighbours;
-            if(slate[bottom][right] == 1)
+            if (slate[bottom][right] == 1)
                 ++neighbours;
             val_neighbour[i][j] = neighbours;
         }
     }
-    for(int i=0; i<row; i++)
+    for (int i = 0; i < row; i++)
     {
-        for(int j=0; j<column; j++)
+        for (int j = 0; j < column; j++)
         {
 
-            if(val_neighbour[i][j] > 3 && slate[i][j] == 1)                       // rule 1
+            if (val_neighbour[i][j] > 3 && slate[i][j] == 1) // rule 1
                 slate[i][j] = 2;
-            if(val_neighbour[i][j] < 2 && slate[i][j] == 1)                       // rule 2
+            if (val_neighbour[i][j] < 2 && slate[i][j] == 1) // rule 2
                 slate[i][j] = 2;
-            if((val_neighbour[i][j] == 2 || val_neighbour[i][j] == 3) && slate[i][j] == 1) // rule 3
+            if ((val_neighbour[i][j] == 2 || val_neighbour[i][j] == 3) && slate[i][j] == 1) // rule 3
                 slate[i][j] = 1;
-            if(val_neighbour[i][j] == 3 && (slate[i][j] == 2 || slate[i][j] == 3))                     // rule 4
+            if (val_neighbour[i][j] == 3 && (slate[i][j] == 2 || slate[i][j] == 3)) // rule 4
                 slate[i][j] = 1;
+        }
+    }
+}
+
+bool validateArgs(int argc, char **argv)
+{
+    if (argc > 2)
+    {
+        return false;
+    }
+    std::ifstream file(argv[1]);
+    if (!file)
+    {
+        return false;
+    }
+    return true;
+}
+
+void init_slate_from_file(char *path)
+{
+    std::ifstream file(path);
+    std::string line;
+    std::vector<std::string> vec_lines;
+    if (file.is_open())
+    {
+        while (getline(file, line))
+        {
+            vec_lines.push_back(line);
+        }
+        file.close();
+    }
+
+    int min_str_size = INT8_MAX;
+    for (auto str : vec_lines)
+    {
+        if (min_str_size > str.size())
+        {
+            min_str_size = str.size();
+        }
+    }
+
+    row = vec_lines.size();
+    column = min_str_size;
+    slate.resize(row);
+    for (int i = 0; i < row; ++i)
+    {
+        slate[i].resize(column);
+        for (int j = 0; j < column; ++j)
+        {
+            if (vec_lines[i][j] == '1')
+            {
+                slate[i][j] = 1;
+            }
+            else if (vec_lines[i][j] == '2')
+            {
+                slate[i][j] = 2;
+            }
+            else
+            {
+                slate[i][j] = 3;
+            }
         }
     }
 }
